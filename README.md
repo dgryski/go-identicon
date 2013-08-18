9f0f181d6722698b0f6ebb092156e6a5e54ce699
Damian Gryski <damian@gryski.com>
2013-08-18 12:35:25 +0200

    upgrade demo to webservice

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## identicon
 
 This program implements a simple [identicon](https://en.wikipedia.org/wiki/Identicon) library, similar to [Github's](https://github.com/blog/1586-identicons).
+
+There is a demo webservice included.  Try:
+
+    go run demo/main.go
+
+and then visit http://localhost:8080/dgryski.png
--- demo/main.go
+++ demo/main.go
@@ -3,30 +3,52 @@ package main
 import (
 	"github.com/dgryski/go-identicon"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 )
 
-func main() {
+func handler(w http.ResponseWriter, r *http.Request) {
 
-	if len(os.Args) != 2 {
-		log.Fatal("usage: demo \"text to hash\"")
+	args := strings.Split(r.URL.Path, "/")
+	args = args[1:]
+
+	if len(args) != 1 {
+		http.Error(w, "", http.StatusBadRequest)
+		return
 	}
+
+	item := args[0]
+
+	// support jpg too?
+	if !strings.HasSuffix(item, ".png") {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	item = strings.TrimSuffix(item, ".png")
 
 	key := []byte{0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF}
 	icon := identicon.New7x7(key)
 
-        log.Printf("creating identicon for '%s'\n", os.Args[1])
+	log.Printf("creating identicon for '%s'\n", item)
 
-	data := []byte(os.Args[1])
-        pngdata := icon.Render(data)
-        
-        log.Println("writing output.png")
-        f, err := os.Create("output.png")
-        defer f.Close()
-        if err != nil {
-            log.Fatalf("cannot create output.png: %s\n", err)
+	data := []byte(item)
+	pngdata := icon.Render(data)
 
-        }
+	w.Header().Set("Content-Type", "image/png")
+	w.Write(pngdata)
 
-        f.Write(pngdata)
+	return
+}
+
+func main() {
+	port := ":8080"
+	if p := os.Getenv("PORT"); p != "" {
+		port = ":" + p
+	}
+	log.Println("Listening on port", port)
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
